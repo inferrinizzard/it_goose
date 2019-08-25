7257d1d8dfc3e1f33c6f0fbfda424df01a9f5590
inferrinizzard <inferrinizzard@gmail.com>
2019-08-25 02:06:50 -0700

    init typewriter

--- js/run.js
+++ js/run.js
@@ -2,3 +2,4 @@ game.state.add("MainMenu", MainMenu);
 game.state.add("Meeting", Meeting);
 game.state.add("TypeWriter", TypeWriter);
 game.state.start("MainMenu");
+// game.state.start("TypeWriter");
--- js/typewriter.js
+++ js/typewriter.js
@@ -1,4 +1,21 @@
+var spawnerT;
+
 class TypeWriter extends Phaser.State {
-	create = () => {};
+	create = () => {
+		spawnerT = setTimeout(() => {
+			const callback = () => {
+				console.log(game.time.totalElapsedSeconds());
+
+				spawnerT = setTimeout(
+					callback,
+					(Math.round(Math.random() * 6) + 1) * 500
+				);
+			};
+			spawnerT = setTimeout(
+				callback,
+				(Math.round(Math.random() * 6) + 1) * 500
+			);
+		}, (Math.round(Math.random() * 6) + 1) * 500);
+	};
 	update = () => {};
 }
